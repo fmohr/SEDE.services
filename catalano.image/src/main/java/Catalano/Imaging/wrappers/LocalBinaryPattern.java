4057499bb2b1cbf20e6f617ab8d39cca0cfc9d4d
Amin F <aminfaez@mail.upb.de>
2019-02-02 22:46:12 +0100

    Added OVERLAY C services - LocalBinaryPattern now transforms images to grayscale before computing features

--- catalano.image/src/main/java/Catalano/Imaging/wrappers/LocalBinaryPattern.java
+++ catalano.image/src/main/java/Catalano/Imaging/wrappers/LocalBinaryPattern.java
@@ -12,9 +12,14 @@ public class LocalBinaryPattern {
 	public LocalBinaryPattern() {
 		localBinaryPattern = new Catalano.Imaging.Texture.BinaryPattern.LocalBinaryPattern();
 	}
+
 	public ImageHistogram ComputeFeatures(FastBitmap fastBitmap) {
+		if(!fastBitmap.isGrayscale()) {
+			fastBitmap.toGrayscale();
+		}
 		return localBinaryPattern.ComputeFeatures(fastBitmap);
 	}
+
 	public List<ImageHistogram> ComputeFeatureSet(List<FastBitmap> fbList) {
 		return fbList.stream().map(this::ComputeFeatures).collect(Collectors.toList());
 	}
